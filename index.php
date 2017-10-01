7a44e7af2c862253e14cabbe852c449d61c7092c
martian6 <miguel.a.martinez01@utrgv.edu>
2017-09-30 19:33:35 -0500

    Changed to php

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Attendance tracking made easy - marStats</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="styles.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top" style="background-color: #1595c4;">
+      <a class="navbar-brand" href="#">Paladin</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http//example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Menu</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Log In</a>
+              <a class="dropdown-item" href="#">Apps</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+    <div class="container">
+
+    	<div class="jumbotron">
+		  <h1 class="display-3">Attendance Tracking and Management Made Easy</h1>
+		  <p class="lead">Keep track of your organization's attendance and visualize your performance like never before.</p>
+		  <hr class="my-4">
+		  <p>Sign up now and get started for only $40/yr.</p>
+		  <p class="lead">
+		    <a class="btn btn-primary btn-lg" href="https://www.google.com" role="button">Sign Up Now</a>
+		  </p>
+		</div>
+
+    	<div class="container">
+        <!-- Example row of columns -->
+	       	<div class="row">
+
+	            <div class="col-md-4">
+	              <h3>Perfect for managers</h3>
+	             <p>Paladin Attendance Tracker is perfect for managers of small businesses or educational organizations. You or your staff input your attendance numbers and we do the rest. No need to create or analyze graphs. It's all taken care of so you can focus on your day to day operations and review your attendance analysis whenever you need.</p>
+	            </div>
+
+	            <div class="col-md-4">
+	              <h3>Portable and reliable</h3>
+	              <p>Available online or on the go, just download our app on your tablet or smartphone and link your account. All your information is updated concurrently across your devices and is updated in real time.</p>
+	            </div>
+
+	            <div class="col-md-4">
+	              <h3>Easy and simple to use</h3>
+	              <p>Just input your attendance records and we'll take care of the rest. Graphs and charts based off of your daily, weekly, monthly, and even yearly attendance records are created instantly.</p>
+	            </div>
+
+	        </div>
+      	</div>
+
+      	<div class="card text-center">
+			  <div class="card-header">
+			    Still not convinced?
+			  </div>
+			  <div class="card-body">
+			    <h4 class="card-title">Try us for free!</h4>
+			    <p class="card-text">Click the button below to start your month-long free trial.</p>
+			    <a href="https://www.google.com" class="btn btn-primary">Start</a>
+			  </div>
+			  <div class="card-footer text-muted">
+			    
+			  </div>
+			</div>
+
+      	<footer class="footer">
+        	<p>&copy; Paladin Attendance Tracker 2017</p>
+      	</footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="assets/js/vendor/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
